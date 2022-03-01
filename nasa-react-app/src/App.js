ac5763eae1274795f35197db996313284bea6076
Ellen Harris <ellenharris@Ellens-MacBook-Air.local>
2022-03-01 13:05:22 +0000

    got bootstrap working

--- nasa-react-app/src/App.js
+++ nasa-react-app/src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
-import Header from "./components/Header";
-import UserInput from "./components/UserInput";
-import Map from "./components/Map";
-import { useState } from "react";
+import './App.css';
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Map from './components/Map';
+import { useState } from 'react';
 
 function App() {
-  const [userInput, setUserInput] = useState({startYear: 1600,
-  endYear: 2022});
+  const [userInput, setUserInput] = useState({
+    startYear: 1600,
+    endYear: 2022,
+  });
 
   return (
     <div>
--- nasa-react-app/src/components/UserInput.js
+++ nasa-react-app/src/components/UserInput.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
 
 const UserInput = ({ setUserInput }) => {
   const year = new Date().getFullYear();
@@ -17,8 +18,9 @@ const UserInput = ({ setUserInput }) => {
   }, [startYear, endYear]);
 
   return (
-    <div>
+    <Container className="mt-2">
       <Form.Select onChange={(event) => setStartYear(event.target.value)}>
+        <option value={yearsArray.splice(-1)}>Select Start Year</option>
         {yearsArray.map((year) => {
           return (
             <option key={year} value={year}>
@@ -29,6 +31,7 @@ const UserInput = ({ setUserInput }) => {
         (
       </Form.Select>
       <Form.Select onChange={(event) => setEndYear(event.target.value)}>
+        <option value={yearsArray.splice(0, 1)}>Select End Year</option>
         {yearsArray.map((year) => {
           return (
             <option key={year} value={year}>
@@ -38,9 +41,8 @@ const UserInput = ({ setUserInput }) => {
         })}
         (
       </Form.Select>
-
       <Form.Label>Input date range</Form.Label>
-    </div>
+    </Container>
   );
 };
 
