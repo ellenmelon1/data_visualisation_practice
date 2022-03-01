a4edfeb893e6a200cfd0ebe1b8d6ef29a92f6259
Ellen Harris <ellenharris@Ellens-MacBook-Air.local>
2022-03-01 11:32:52 +0000

    started map feature using leaflet

--- nasa-react-app/src/components/Map.js
+++ nasa-react-app/src/components/Map.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Map.css';
 
 function Map({ userInput }) {
   const { startYear, endYear } = userInput;
-  
+
   const [isLoading, setIsLoading] = useState(true);
   const [meteoriteData, setMeteoriteData] = useState([]);
 
@@ -19,16 +21,38 @@ function Map({ userInput }) {
   }, [startYear, endYear]);
 
   const mapOutput = isLoading ? (
-    <h3>LOADING</h3>
+    <Marker position={[51.505, -0.09]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
   ) : (
-    <ul>
-      {meteoriteData.map((data) => {
-        return <li key={data.name}>{data.name}</li>;
-      })}
-    </ul>
+    meteoriteData.slice(0, 5).map((data) => {
+      {
+        console.log(typeof Number(data.reclong));
+      }
+      return (
+        <Marker
+          key={data.id}
+          position={[Number(data.reclong), Number(data.reclat)]}
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      );
+    })
   );
 
-  return <div>{mapOutput}</div>;
+  return (
+    <MapContainer center={[51.505, -0.09]} zoom={2}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {mapOutput}
+    </MapContainer>
+  );
 }
 
 export default Map;
