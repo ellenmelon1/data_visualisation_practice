2e5b3125b1a96a4cb767d1da051b238716be95db
Ellen Harris <ellenharris@192.168.1.112>
2022-04-25 12:45:55 +0100

    added inline styling for custom fonts to see if netlify picks up on them

--- nasa-react-app/src/components/Header.js
+++ nasa-react-app/src/components/Header.js
@@ -3,12 +3,19 @@ import '../App.css';
 const Header = () => {
   return (
     <div>
-      <h1 className="header">NASA Meteorite Landings</h1>
-      <h3 className="instructions">
+      <h1 className="header" style={{ fontFamily: 'Distant Galaxy' }}>
+        NASA Meteorite Landings
+      </h1>
+      <h3 className="instructions" style={{ fontFamily: 'Distant Galaxy' }}>
         Filter the data to show the location of recorded meteorite landings on
         the map
       </h3>
-      <p className="default_inputs_label">Default values are 2010 - 2012</p>
+      <p
+        className="default_inputs_label"
+        style={{ fontFamily: 'Distant Galaxy' }}
+      >
+        Default values are 2010 - 2012
+      </p>
     </div>
   );
 };
